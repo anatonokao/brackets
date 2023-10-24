884590d13eb70129748583a5192ea65ef6c92b1a
Alexandr <anato.devs@gmail.com>
2023-10-24 17:26:25 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig = bracketsConfig.map(config => config.join(''))
+  console.log(bracketsConfig)
+  let lastLength = str.length
+  while (str.length) {
+    bracketsConfig.forEach(config => {
+      str = str.replaceAll(config, '')
+    })
+    if (lastLength === str.length) return false
+    lastLength = str.length
+  }
+  return true
 }
